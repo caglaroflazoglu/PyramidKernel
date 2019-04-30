1021490504ecb2c24b5961e8daea94f7c7d6bb18
furkanonder <furkantahaonder@gmail.com>
2019-04-30 19:11:05 +0300

    malloc function implemented

--- kernel/kernel_main.c
+++ kernel/kernel_main.c
@@ -7,11 +7,20 @@
 #include "serial.h"
 #include "string.h"
 #include "isr.h"
+#include "heap.h"
 
 void main() {
 	clear_screen();
 	init_descriptor_tables();
 	welcome_text();   
+
+	uint32_t a = kmalloc(8);
+	uint32_t b = kmalloc(8);
+	uint32_t c = kmalloc(8);
+	printf("a: %x\n", a);
+	printf("b: %x\n", b);
+	printf("c: %x\n", c);
+	
 	init_prompt();
 	init_keyboard();
 	return;
