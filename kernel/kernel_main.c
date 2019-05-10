274683e4592ef43423096989272b2c6763fe86e6
furkanonder <furkantahaonder@gmail.com>
2019-05-10 17:37:14 +0300

    paging added

--- kernel/kernel_main.c
+++ kernel/kernel_main.c
@@ -1,27 +1,33 @@
 #include <stdint.h>
 
-#include "descriptor_tables.h"
-#include "framebuffer.h"
-#include "keyboard.h"
-#include "multiboot.h"
-#include "serial.h"
-#include "string.h"
-#include "isr.h"
-#include "heap.h"
+#include "drivers/descriptor_tables.h"
+#include "lib/framebuffer.h"
+#include "drivers/keyboard.h"
+#include "lib/multiboot.h"
+#include "drivers/serial.h"
+#include "lib/string.h"
+#include "mm/paging.h"
+#include "drivers/isr.h"
+#include "mm/kheap.h"
+
 
 void main() {
-	clear_screen();
-	init_descriptor_tables();
-	welcome_text();   
+   clear_screen();
+   welcome_text();
+   init_descriptor_tables();
+   init_paging();
+   init_keyboard();
 
-	uint32_t a = kmalloc(8);
-	uint32_t b = kmalloc(8);
-	uint32_t c = kmalloc(8);
-	printf("a: %x\n", a);
-	printf("b: %x\n", b);
-	printf("c: %x\n", c);
-	
-	init_prompt();
-	init_keyboard();
-	return;
+   uint32_t a = kmalloc(8);
+   uint32_t b = kmalloc(8);
+   uint32_t c = kmalloc(8);
+   printf("a: %x\n", a);
+   printf("b: %x\n", b);
+   printf("c: %x\n", c);
+   
+   init_prompt();
+   uint32_t *ptr = (uint32_t *)0xA0000000;
+   uint32_t do_page_fault = *ptr;
+
+   return;
 }
